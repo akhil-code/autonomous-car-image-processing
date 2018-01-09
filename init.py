8c3da90e09fa92070f1343fe7020093f8cd39219
guttula akhil <guttula.akhil@gmail.com>
2018-01-09 17:34:40 +0530

    accurate reading obtained

--- init.py
+++ init.py
@@ -21,8 +21,7 @@ def seg_intersect(A,B,C,D):
  
     if (determinant == 0):
         #The lines are parallel. This is simplified
-        #by returning a pair of FLT_MAX
-        return (99999,99999)
+        return (np.inf,np.inf)
     else:
         x = (b2*c1 - b1*c2)/determinant;
         y = (a1*c2 - a2*c1)/determinant;
@@ -35,8 +34,9 @@ def movingAverage(avg, new_sample, N=20):
     avg += new_sample / N;
     return avg;
 
-def draw_lines(img, lines, color=[255,0,0],thickness=2):
+def draw_lines(img,srcImg, lines, color=[255,0,0],thickness=2):
     global avgLeft,avgRight
+    print 'length: '+str(len(lines))
 
     # state variables to keep track of most dominant segment
     largestLeftLineSize = 0
@@ -51,20 +51,22 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
         cv2.line(img, (int(avgx1), int(avgy1)),(int(avgx2),int(avgy2)),[255,255,255],12) #draw right line
         return
 
+
     for line in lines:
         for x1,y1,x2,y2 in line:
             size = math.hypot(x2 - x1, y2 - y1)
-            slope = ((y2-y1)/(x2-x1))
+            slope = ((float(y2)-y1)/(float(x2)-x1))
+
             # Filter slope based on incline and
             # find the most dominent segment based on length
             if (slope > 0.5): #right
                 if (size > largestRightLineSize):
                     largestRightLine = (x1,y1,x2,y2)
-                cv2.line(img,(x1,y1),(x2, y2),color,thickness)
+                cv2.line(img,(x1,y1),(x2, y2),color,1)
             elif (slope < -0.5): #left
                 if (size > largestLeftLineSize):
                     largestLeftLine = (x1,y1,x2,y2)
-                cv2.line(img,(x1,y1),(x2,y2),color,thickness)
+                cv2.line(img,(x1,y1),(x2,y2),color,1)
 
     # Define an imaginary horizontal line in the center of the screen
     # and at the bottom of the image, to extrapolate determined segment
@@ -82,13 +84,13 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
     upLeftPoint = seg_intersect(upLinePoint1,upLinePoint2, p3,p4)
     downLeftPoint = seg_intersect(downLinePoint1,downLinePoint2, p3,p4)
 
-    if (math.isnan(upLeftPoint[0]) or math.isnan(downLeftPoint[0])):
+    if (np.isinf(upLeftPoint[0]) or np.isinf(downLeftPoint[0])):
         avgx1, avgy1, avgx2, avgy2 = avgLeft
         cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
         avgx1, avgy1, avgx2, avgy2 = avgRight
         cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
         return
-    cv2.line(img, (int(upLeftPoint[0]), int(upLeftPoint[1])), (int(downLeftPoint[0]), int(downLeftPoint[1])), [0, 0, 255], 8) #draw left line
+    # cv2.line(img, (int(upLeftPoint[0]), int(upLeftPoint[1])), (int(downLeftPoint[0]), int(downLeftPoint[1])), [0, 0, 255], 8) #draw left line
 
     # Calculate the average position of detected left lane over multiple video frames and draw
     avgx1,avgy1,avgx2,avgy2 = avgLeft
@@ -102,13 +104,13 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
     p6 = np.array( [largestRightLine[2], largestRightLine[3]] )
     upRightPoint = seg_intersect(upLinePoint1,upLinePoint2,p5,p6)
     downRightPoint = seg_intersect(downLinePoint1,downLinePoint2,p5,p6)
-    if (math.isnan(upRightPoint[0]) or math.isnan(downRightPoint[0])):
+    if (np.isinf(upRightPoint[0]) or np.isinf(downRightPoint[0])):
         avgx1, avgy1, avgx2, avgy2 = avgLeft
-        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+        # cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
         avgx1, avgy1, avgx2, avgy2 = avgRight
-        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
+        # cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
         return
-    cv2.line(img, (int(upRightPoint[0]), int(upRightPoint[1])), (int(downRightPoint[0]), int(downRightPoint[1])), [0, 0, 255], 8) #draw left line
+    # cv2.line(img, (int(upRightPoint[0]), int(upRightPoint[1])), (int(downRightPoint[0]), int(downRightPoint[1])), [0, 0, 255], 8) #draw left line
 
     # Calculate the average position of detected right lane over multiple video frames and draw
     avgx1, avgy1, avgx2, avgy2 = avgRight
@@ -117,6 +119,7 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
     cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
 
 def region_of_interest(img,vertices):
+    vertices = np.array([vertices],dtype=np.int32)
     mask = np.zeros(img.shape,np.uint8)
     if len(img.shape) > 2:
         channel_count = img.shape[2]
@@ -128,10 +131,10 @@ def region_of_interest(img,vertices):
     masked_image = cv2.bitwise_and(img,mask)
     return masked_image
 
-def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+def hough_lines(img,srcImg,rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
     line_img = np.zeros(img.shape, dtype=np.uint8)
-    draw_lines(line_img, lines)
+    draw_lines(line_img,srcImg,lines)
     return line_img
 
 
@@ -140,27 +143,22 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
 
 #read image
 img = cv2.imread('lane.jpeg')
-cv2.imshow('input',img)
-cv2.waitKey(0)
-
 #apply blur
 blurImg = cv2.GaussianBlur(img,(11,11),0) #kernel size 11x11
-cv2.imshow('blurred input',blurImg)
-cv2.waitKey(0)
-
 #canny edge detection
 edgesImage = cv2.Canny(blurImg,40,50)
-cv2.imshow('canny',edgesImage)
-cv2.waitKey(0)
 
 #dimensions of image
 imgHeight,imgWidth,_ = img.shape
-vertices = np.array([[[3*imgWidth/4,3*imgHeight/5],[imgWidth/4,3*imgHeight/5],[40,imgHeight],[imgWidth-40,imgHeight]]],dtype=np.int32)
+# vertices = np.array([[[3*imgWidth/4,3*imgHeight/5],[imgWidth/4,3*imgHeight/5],[40,imgHeight],[imgWidth-40,imgHeight]]],dtype=np.int32)
+vertices = [[3*imgWidth/4,3*imgHeight/5],[imgWidth/4,3*imgHeight/5],[40,imgHeight],[imgWidth-40,imgHeight]]
 
 roi = region_of_interest(edgesImage,vertices)
+lineMarkedImage = hough_lines(roi,img,1, np.pi/180, 40, 30, 200)
+
+
 cv2.imshow('roi',roi)
 cv2.waitKey(0)
 
-lineMarkedImage = hough_lines(roi, 1, np.pi/180, 40, 30, 200)
 cv2.imshow('hough lines',lineMarkedImage)
 cv2.waitKey(0)
