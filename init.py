26b7f3ca10c5d9bcb4292fbf9e88a519abd60b13
guttula akhil <guttula.akhil@gmail.com>
2018-01-09 15:30:29 +0530

    processing on image done

--- init.py
+++ init.py
@@ -6,27 +6,27 @@ import cv2,math
 avgLeft = (0, 0, 0, 0)
 avgRight = (0, 0, 0, 0)
 
-
-def perp( a ) :
-    b = np.empty_like(a)
-    b[0] = -a[1]
-    b[1] = a[0]
-    return b
-
-# line segment a given by endpoints a1, a2
-# line segment b given by endpoints b1, b2
-# return
-def seg_intersect(a1,a2, b1,b2):
-    da = a2-a1
-    db = b2-b1
-    dp = a1-b1
-    dap = perp(da)
-    denom = np.dot( dap, db)
-    num = np.dot( dap, dp )
-    try:
-        return (num / denom.astype(float))*db + b1
-    except:
-        return (num / (denom.astype(float)+1))*db + b1
+def seg_intersect(A,B,C,D):
+    #Line AB represented as a1x + b1y = c1
+    a1 = B[1] - A[1]
+    b1 = A[0] - B[0]
+    c1 = a1*(A[0]) + b1*(A[1])
+ 
+    #Line CD represented as a2x + b2y = c2
+    a2 = D[1] - C[1]
+    b2 = C[0] - D[0]
+    c2 = a2*(C[0])+ b2*(C[1])
+ 
+    determinant = a1*b2 - a2*b1
+ 
+    if (determinant == 0):
+        #The lines are parallel. This is simplified
+        #by returning a pair of FLT_MAX
+        return (99999,99999)
+    else:
+        x = (b2*c1 - b1*c2)/determinant;
+        y = (a1*c2 - a2*c1)/determinant;
+        return (x, y)
 
 def movingAverage(avg, new_sample, N=20):
     if (avg == 0):
@@ -48,8 +48,9 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
         avgx1,avgy1,avgx2,avgy2=avgLeft
         cv2.line(img,(int(avgx1),int(avgy1)),(int(avgx2),int(avgy2)),[255,255,255],12) #draw left line
         avgx1,avgy1,avgx2,avgy2=avgRight
-        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw right line
+        cv2.line(img, (int(avgx1), int(avgy1)),(int(avgx2),int(avgy2)),[255,255,255],12) #draw right line
         return
+
     for line in lines:
         for x1,y1,x2,y2 in line:
             size = math.hypot(x2 - x1, y2 - y1)
@@ -58,27 +59,29 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
             # find the most dominent segment based on length
             if (slope > 0.5): #right
                 if (size > largestRightLineSize):
-                    largestRightLine = (x1, y1, x2, y2)
-                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+                    largestRightLine = (x1,y1,x2,y2)
+                cv2.line(img,(x1,y1),(x2, y2),color,thickness)
             elif (slope < -0.5): #left
                 if (size > largestLeftLineSize):
-                    largestLeftLine = (x1, y1, x2, y2)
-                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+                    largestLeftLine = (x1,y1,x2,y2)
+                cv2.line(img,(x1,y1),(x2,y2),color,thickness)
 
     # Define an imaginary horizontal line in the center of the screen
     # and at the bottom of the image, to extrapolate determined segment
-    imgHeight, imgWidth = (img.shape[0], img.shape[1])
-    upLinePoint1 = np.array( [0, int(imgHeight - (imgHeight/3))] )
-    upLinePoint2 = np.array( [int(imgWidth), int(imgHeight - (imgHeight/3))] )
-    downLinePoint1 = np.array( [0, int(imgHeight)] )
-    downLinePoint2 = np.array( [int(imgWidth), int(imgHeight)] )
+    imgHeight,imgWidth = img.shape
+    upLinePoint1 = np.array( [0, int((2.0/3.0)*imgHeight)] )
+    upLinePoint2 = np.array( [imgWidth,int((2.0/3.0)*imgHeight)] )
+    downLinePoint1 = np.array([0,imgHeight])
+    downLinePoint2 = np.array([imgWidth,imgHeight])
 
     # Find the intersection of dominant lane with an imaginary horizontal line
     # in the middle of the image and at the bottom of the image.
     p3 = np.array( [largestLeftLine[0], largestLeftLine[1]] )
     p4 = np.array( [largestLeftLine[2], largestLeftLine[3]] )
+    
     upLeftPoint = seg_intersect(upLinePoint1,upLinePoint2, p3,p4)
     downLeftPoint = seg_intersect(downLinePoint1,downLinePoint2, p3,p4)
+
     if (math.isnan(upLeftPoint[0]) or math.isnan(downLeftPoint[0])):
         avgx1, avgy1, avgx2, avgy2 = avgLeft
         cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
@@ -88,17 +91,17 @@ def draw_lines(img, lines, color=[255,0,0],thickness=2):
     cv2.line(img, (int(upLeftPoint[0]), int(upLeftPoint[1])), (int(downLeftPoint[0]), int(downLeftPoint[1])), [0, 0, 255], 8) #draw left line
 
     # Calculate the average position of detected left lane over multiple video frames and draw
-    avgx1, avgy1, avgx2, avgy2 = avgLeft
-    avgLeft = (movingAverage(avgx1, upLeftPoint[0]), movingAverage(avgy1, upLeftPoint[1]), movingAverage(avgx2, downLeftPoint[0]), movingAverage(avgy2, downLeftPoint[1]))
-    avgx1, avgy1, avgx2, avgy2 = avgLeft
-    cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
+    avgx1,avgy1,avgx2,avgy2 = avgLeft
+    avgLeft=(movingAverage(avgx1,upLeftPoint[0]),movingAverage(avgy1,upLeftPoint[1]),movingAverage(avgx2,downLeftPoint[0]),movingAverage(avgy2,downLeftPoint[1]))
+    avgx1,avgy1,avgx2,avgy2 = avgLeft
+    cv2.line(img,(int(avgx1),int(avgy1)),(int(avgx2),int(avgy2)),[255,255,255],12) #draw left line
 
     # Find the intersection of dominant lane with an imaginary horizontal line
     # in the middle of the image and at the bottom of the image.
     p5 = np.array( [largestRightLine[0], largestRightLine[1]] )
     p6 = np.array( [largestRightLine[2], largestRightLine[3]] )
-    upRightPoint = seg_intersect(upLinePoint1,upLinePoint2, p5,p6)
-    downRightPoint = seg_intersect(downLinePoint1,downLinePoint2, p5,p6)
+    upRightPoint = seg_intersect(upLinePoint1,upLinePoint2,p5,p6)
+    downRightPoint = seg_intersect(downLinePoint1,downLinePoint2,p5,p6)
     if (math.isnan(upRightPoint[0]) or math.isnan(downRightPoint[0])):
         avgx1, avgy1, avgx2, avgy2 = avgLeft
         cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
