de8698033844494d7c0ca4f78d323f5ecb9445c0
guttula akhil <guttula.akhil@gmail.com>
2018-01-10 09:37:10 +0530

    update

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2,math
+import cv2,math,sys
 
 
 # reset global state of average values
@@ -55,8 +55,11 @@ def draw_lines(img,srcImg, lines, color=[255,0,0],thickness=2):
     for line in lines:
         for x1,y1,x2,y2 in line:
             size = math.hypot(x2 - x1, y2 - y1)
-            slope = ((float(y2)-y1)/(float(x2)-x1))
-
+            
+            if x2 != x1:
+                slope = ((float(y2)-y1)/(float(x2)-x1))
+            else:
+                continue
             # Filter slope based on incline and
             # find the most dominent segment based on length
             if (slope > 0.5): #right
@@ -118,6 +121,9 @@ def draw_lines(img,srcImg, lines, color=[255,0,0],thickness=2):
     avgx1, avgy1, avgx2, avgy2 = avgRight
     cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255,255,255], 12) #draw left line
 
+    return (avgLeft,avgRight)
+
+
 def region_of_interest(img,vertices):
     vertices = np.array([vertices],dtype=np.int32)
     mask = np.zeros(img.shape,np.uint8)
@@ -134,14 +140,19 @@ def region_of_interest(img,vertices):
 def hough_lines(img,srcImg,rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
     line_img = np.zeros(img.shape, dtype=np.uint8)
-    draw_lines(line_img,srcImg,lines)
-    return line_img
+    lane = draw_lines(line_img,srcImg,lines)
+    return line_img,lane
 
+def getMidPoint(point1,point2):
+    return ((point1[0]+point2[0])/2,(point1[1]+point1[1])/2)
 
 ################################################################################################################################
 
+video_file = 'video1.mp4'
+if len(sys.argv) > 1:
+    video_file = sys.argv[1]
 
-cap = cv2.VideoCapture('video3.mp4')
+cap = cv2.VideoCapture(video_file)
 
 while True:
     #read image
@@ -156,19 +167,46 @@ while True:
 
     #dimensions of image
     imgHeight,imgWidth,_ = img.shape
-    # vertices = np.array([[[3*imgWidth/4,3*imgHeight/5],[imgWidth/4,3*imgHeight/5],[40,imgHeight],[imgWidth-40,imgHeight]]],dtype=np.int32)
     vertices = [[3*imgWidth/4,3*imgHeight/5],[imgWidth/4,3*imgHeight/5],[40,imgHeight],[imgWidth-40,imgHeight]]
 
     roi = region_of_interest(edgesImage,vertices)
-    lineMarkedImage = hough_lines(roi,img,1, np.pi/180, 40, 30, 200)
+    lineMarkedImage,lane = hough_lines(roi,img,1, np.pi/180, 40, 30, 200)
 
+    if lane is None:
+        continue
+
+    leftLane,rightLane = lane
+
+    referenceLine = ((0,int(0.8*imgHeight)),(imgWidth,int(0.8*imgHeight)))
+    # cv2.line(lineMarkedImage,referenceLine,[255,255,255],12)#draw left line
+
+    upperPoint = leftLane[0:2]
+    lowerPoint = leftLane[2:4]
+    p1 = seg_intersect(upperPoint,lowerPoint,referenceLine[0],referenceLine[1])
+
+    upperPoint = rightLane[0:2]
+    lowerPoint = rightLane[2:4]
+    p2 = seg_intersect(upperPoint,lowerPoint,referenceLine[0],referenceLine[1])
+
+
+    cv2.line(img,p1,p2,[0,0,255],12)
+    p3,p4 = ((0,referenceLine[1][1]),(imgWidth,referenceLine[1][1]))
+    carPoint = getMidPoint(p3,p4)
+    lanePoint = getMidPoint(p1,p2)
+    cv2.circle(img,lanePoint,10,(0,0,255),-1)
+    cv2.circle(img,carPoint,15,(255,0,0),-1)
+
+    # if carPoint[0]<lanePoint[0]:
+    #     print 'left'
+    # else:
+    #     print 'right'
 
     cv2.imshow('input',img)
     # cv2.imshow('roi',roi)
     # cv2.waitKey(0)
 
-    cv2.imshow('hough lines',lineMarkedImage)
-    cv2.waitKey(10)
+    # cv2.imshow('hough lines',lineMarkedImage)
+    cv2.waitKey(20)
 
 print 'video completed'
 cv2.destroyAllWindows()
